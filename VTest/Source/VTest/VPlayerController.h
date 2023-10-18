0d54e4c918c5400287503b073e5da2acade788d1
Yoosung-H <101240036+Hongyoosung@users.noreply.github.com>
2023-10-18 23:49:17 +0900

    1018

---
+++ VTest/Source/VTest/VPlayerController.h
@@ -0,0 +1,16 @@
+// Copyright 2022, Infima Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "VPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VTEST_API AVPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
